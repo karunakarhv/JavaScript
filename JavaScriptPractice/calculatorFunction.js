22548c6c2e4cc35d4903313aeb2ff3e8d5e16294
Karunakar <hvkarunakar@gmail.com>
2019-11-28 20:36:33 +1100

    First Push

---
+++ JavaScriptPractice/calculatorFunction.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+function add(x , y){
+	document.getElementById("result").innerHTML =  x + y;
+}
+
+function sub(x , y){
+	document.getElementById("result").innerHTML = x - y;
+}
+
+function mul(x , y){
+	document.getElementById("result").innerHTML =  x * y;
+}
+
+function div(x , y){
+	document.getElementById("result").innerHTML =  x/y;
+}
+
+function equalsTo(){
+	
+}
+
+function displayNumber(x) {
+	document.getElementById("result").innerHTML = x;
+}
+
+function displayOperator(x) {
+	document.getElementById("result").innerHTML = x;
+}
